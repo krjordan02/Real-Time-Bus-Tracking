c74d43ee60cecb1a44192ecc4cccf9377ee1914c
krjordan02 <keeganrjordan@gmail.com>
2023-12-01 11:36:16 -0700

    All bus update

--- mapanimation.js
+++ mapanimation.js
@@ -1,49 +1,71 @@
-// This array contains the coordinates for all bus stops between MIT and Harvard
-const busStops = [
-  [-71.093729, 42.359244],
-  [-71.094915, 42.360175],
-  [-71.0958, 42.360698],
-  [-71.099558, 42.362953],
-  [-71.103476, 42.365248],
-  [-71.106067, 42.366806],
-  [-71.108717, 42.368355],
-  [-71.110799, 42.369192],
-  [-71.113095, 42.370218],
-  [-71.115476, 42.372085],
-  [-71.117585, 42.373016],
-  [-71.118625, 42.374863],
-];
+//gets the longitude and latitude of each bus
+async function getCoordinates(data){
+    let coordinates = [];
+    data.forEach(bus => {
+        let lat = bus.attributes.latitude;
+        let lon = bus.attributes.longitude;
+        let coor = [lon, lat];
+        coordinates.push(coor);
+    });
+    return coordinates;
+}
 
-// TODO: add your own access token
+//gets location of each bus and returns an array with their logitude and latitude
+async function getBusLocations(){
+    const url = 'https://api-v3.mbta.com/vehicles';
+    const resp = await fetch(url);
+    const json = await resp.json();
+    const data = getCoordinates(json.data);
+    return data;
+}
+
+//adds each bus to the map
+async function addBusToMap(coordinates){
+    let counter = 0;
+    let markers = [];
+    coordinates.forEach(coor => {
+        let marker = new mapboxgl.Marker()
+            .setLngLat([coor[0], coor[1]])
+            .addTo(map);
+        markers.push(marker);
+        counter++;
+    })
+    console.log(markers);
+    return markers;
+}
+
+//updates the location of each bus and changes the coordinates of the marker
+async function updateBusMap(coordinates, markers){
+    let counter = 0;
+    markers.forEach(marker => {
+        marker.setLngLat([coordinates[counter][0], coordinates[counter][1]]);
+        counter++;
+    })
+}
+
+//mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2VlZ2FucmpvcmRhbiIsImEiOiJjbHBrY2J3ancwNnV3MmpvOWF5eWE2b3Q0In0.A-6c8W5JX6_sFjaVHzgGtA';
 
 // This is the map instance
 let map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-71.104081, 42.365554],
-  zoom: 14,
+    container: 'map',
+    style: 'mapbox://styles/mapbox/navigation-night-v1',
+    center: [-71.104081, 42.365554],
+    zoom: 12,
+        anchor: "viewport",
+        color: "white",
+        intensity: 0.4
 });
 
-// TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
-  let marker = new mapboxgl.Marker()
-    .setLngLat([-71.093729, 42.359244])
-    .addTo(map);
+//gets locations of busses and adds markers to the map
+//updates bus locations every 15 seconds
+async function move() {
+    var locations = await getBusLocations();
+    const markers = await addBusToMap(locations);
 
-// counter here represents the index of the current bus stop
-let counter = 0;
-function move() {
-  // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-  // Use counter to access bus stops in the array busStops
-  // Make sure you call move() after you increment the counter.
-  
-  console.log(busStops[counter]);
-  setTimeout(() => {
-    //if(counter < busStops.length){
-      marker.setLngLat(busStops[counter]);
-      counter++;
-      move();
-    //}
-      
-  }, 1000)
+    setInterval(async () => {
+        locations = await getBusLocations();
+        await updateBusMap(locations, markers);
+    }, 15000);
 }
+
